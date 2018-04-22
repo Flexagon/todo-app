e3c0c58dd284f2a57b1ef179e048b830cdd9c5c4
Max_Gavrosh <flexagon89@gmail.com>
2018-04-22 23:47:23 +0300

    Add scss and update start architecture

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: [
     'react-hot-loader/patch',
-    './src/index.js',
+    './src/js/index.js',
+    './src/scss/style.scss',
   ],
   module: {
     rules: [
@@ -17,21 +20,45 @@ module.exports = {
         exclude: /node_modules/,
         use: ['eslint-loader'],
       },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              minimize: true,
+              url: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          }],
+        }),
+      },
     ],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
     new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
     contentBase: './dist',
     hot: true,
-  }
+  },
 };
