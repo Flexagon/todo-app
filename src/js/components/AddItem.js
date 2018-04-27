c67f874dffded0659f9f64ae5c6682f2cf3e24c6
Max_Gavrosh <flexagon89@gmail.com>
2018-04-27 16:16:32 +0300

    Add component AddItem

--- src/js/components/AddItem.js
+++ src/js/components/AddItem.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddItem = () => (
-    <div>
-      <input type="text" />
-      <button>Add ToDo</button>
-    </div>
-);
+export default class AddItem extends Component {
+  constructor(props) {
+    super(props);
 
-export default AddItem;
+    this.state = { inputValue: '' };
+  }
+
+  inputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  onAddItem = (e) => {
+    const { pushToArray } = this.props;
+
+    this.setState({ inputValue: '' });
+    pushToArray(this.state.inputValue);
+  };
+
+  render() {
+    return (
+      <div className="AddItem">
+        <input
+          className="AddItem-input"
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.inputChange}
+        />
+        <button className="AddItem-button" onClick={this.onAddItem}>Add ToDo</button>
+      </div>
+    );
+  }
+}
