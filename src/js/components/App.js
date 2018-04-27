c172e2d8cf6dff85bf87b680e21bae2de38261c1
Max_Gavrosh <flexagon89@gmail.com>
2018-04-27 16:16:55 +0300

    Update component App

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AddItem from './AddItem';
+import ToDoList from './ToDoList';
 
-const App = () => <AddItem />;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    const toDoArray = [
+      { title: 'First Todo' },
+    ];
+
+    this.state = { toDoArray };
+  }
+
+  pushToArray = (title) => {
+    const { toDoArray } = this.state;
+
+    if (title !== '') toDoArray.push({ title });
+    this.setState({ toDoArray });
+  }
+
+  removeFromArray = (title) => {
+    const { toDoArray } = this.state;
+
+    this.setState({ toDoArray: toDoArray.filter(item => item.title !== title) });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddItem pushToArray={this.pushToArray} />
+        <ToDoList toDoArray={this.state.toDoArray} removeFromArray={this.removeFromArray} />
+      </div>
+    );
+  }
+}
